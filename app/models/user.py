817514a902c43d6702b2c3c51829724932448194
Adam Bazzi <adambazzi14@gmail.com>
2023-06-07 13:40:19 -0700

    next

--- app/models/user.py
+++ app/models/user.py
@@ -1,27 +1,36 @@
+
 from .db import db, environment, SCHEMA
 from werkzeug.security import generate_password_hash, check_password_hash
-# from sqlalchemy.schema import CheckConstraint
 from flask_login import UserMixin
-from sqlalchemy import text
-import re  # importing re module
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
-    """A User model that stores user information and handles password hashing."""
     __tablename__ = 'users'
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True, index=True)
-    email = db.Column(db.String(255), nullable=False, unique=True, index=True)
-    phone = db.Column(db.String(255), nullable=False, unique=True)
-    first_name = db.Column(db.String(150), nullable=False)
-    last_name = db.Column(db.String(150), nullable=False)
-    admin = db.Column(db.Boolean, nullable=False, default=False)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    phone = db.Column(db.String(255), nullable=False, unique=True)
+    first_name=db.Column(db.String(50), nullable=False)
+    last_name=db.Column(db.String(50), nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
 
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+        nullable=False
+    )
+
+    # Define Relationships
+    # Define a one-to-many relationship with Tickets for user
+    tickets = db.relationship('Ticket', back_populates='user')
 
     @property
     def password(self):
@@ -35,7 +44,6 @@ class User(db.Model, UserMixin):
         return check_password_hash(self.password, password)
 
     def to_dict(self):
-        """Return user details as a dict."""
         return {
             'id': self.id,
             'username': self.username,
@@ -44,12 +52,5 @@ class User(db.Model, UserMixin):
             'first_name': self.first_name,
             'last_name': self.last_name,
             'admin': self.admin
+
         }
-
-    def __repr__(self):
-        """Represent instance of a User."""
-        return f"<User: {self.username}>"
-
-    # Define Relationships
-    # Define a one-to-many relationship with Tickets for user
-    tickets = db.relationship('Ticket', back_populates='user')
